f6136eefcc6891c5e41fa6562bbd9b2057e99caa
margaridabf1311 <margarida1311ferreira@gmail.com>
2026-02-08 20:09:42 +0000

    Christofides

--- cdp1.py
+++ cdp1.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
+from itertools import combinations
+import ast
 
-df = pd.read_csv("tsp_instances_dataset.csv")
+
+
+df = pd.read_csv("tsp_dataset.csv")
 
 def coordenadas_instancia(row):
-    coords = []
-    i = 1
-    while f"City_{i}_X" in row:
-        x = row[f"City_{i}_X"]
-        y = row[f"City_{i}_Y"]
-
-        if pd.isna(x) or pd.isna(y):
-            break
-
-        coords.append([float(x), float(y)])
-        i += 1
-    return np.array(coords)
+    coords_list = ast.literal_eval(row['city_coordinates'])
+    coords = np.array(coords_list, dtype=float)
+    return coords
 
 
 def vizinho_mais_proximo(coords):
@@ -40,8 +36,63 @@ def vizinho_mais_proximo(coords):
 
     return total_dist
 
-coords0 = coordenadas_instancia(df.loc[0])
+
+
+def distancia_euclideana(a, b):
+    return np.linalg.norm(a - b)
+
+def christofides(coords):
+    n = len(coords)
+    
+    G = nx.Graph()
+    for i in range(n):
+        for j in range(i+1, n):
+            dist = distancia_euclideana(coords[i], coords[j])
+            G.add_edge(i, j, weight=dist)
+    
+    AGM = nx.minimum_spanning_tree(G)
+    odd_nodes = [v for v, d in AGM.degree() if d % 2 == 1]
+    
+    odd_graph = nx.Graph()
+    for u, v in combinations(odd_nodes, 2):
+        odd_graph.add_edge(u, v, weight=distancia_euclideana(coords[u], coords[v]))
+    
+    matching = nx.algorithms.matching.min_weight_matching(odd_graph)
+    multi_graph = nx.MultiGraph(AGM)
+    multi_graph.add_edges_from(matching)
+    odd_left = [v for v, d in multi_graph.degree() if d % 2 == 1]
+
+    while odd_left:
+        u = odd_left.pop()
+        v = min(odd_left, key=lambda x: distancia_euclideana(coords[u], coords[x]))
+        multi_graph.add_edge(u, v, weight=distancia_euclideana(coords[u], coords[v]))
+        odd_left.remove(v)
+
+
+    euler_circuit = list(nx.eulerian_circuit(multi_graph))
+    visited = set()
+    route = []
+    for u, v in euler_circuit:
+        if u not in visited:
+            route.append(u)
+            visited.add(u)
+        if v not in visited:
+            route.append(v)
+            visited.add(v)
+    
+    route.append(route[0])
+    
+    total_dist = sum(distancia_euclideana(coords[route[i]], coords[route[i+1]]) for i in range(len(route)-1))
+    
+    return total_dist
+
+
+coords0 = coordenadas_instancia(df.loc[1294])
 
 distanciavmp = vizinho_mais_proximo(coords0)
 print("Distância 1:", distanciavmp)
+distanciac = christofides(coords0)
+print("Distância 2:", distanciac)
+
+
 
